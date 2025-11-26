7b35e94875115cbc005a3033ae8e14474468e984
Dominik Königseder <koenigsederdominik@gmail.com>
2025-11-26 21:51:49 +0100

    add base css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { INITIAL_STATE } from './data/initialState'
 function App() {
   const [exams, setExams] = useState(INITIAL_STATE)
   return (
-    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+    <div className="app-container">
       <h1>ExamFlow</h1>
       <pre style={{ textAlign: 'left' }}>
         {JSON.stringify(exams, null, 2)}
