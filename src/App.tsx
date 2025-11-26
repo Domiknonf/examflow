a8ac98478aaa6ec10e9441078cd15a8433ea0565
Dominik Königseder <koenigsederdominik@gmail.com>
2025-11-26 22:33:14 +0100

    fix lint error

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { INITIAL_STATE, AppState } from './data/initialState'
 
 function App() {
-  const [exams, setExams] = useState<AppState>(INITIAL_STATE)
+  const [exams /*, setExams */] = useState<AppState>(INITIAL_STATE)
   return (
     <div className="app-container">
       <h1>ExamFlow</h1>
