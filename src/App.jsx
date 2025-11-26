b13e07449956600caef955b34af3f3416867e487
Dominik Königseder <koenigsederdominik@gmail.com>
2025-11-26 19:46:51 +0100

    Create react app with vite

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>ExamFlow</h1>
+      <p>Bereit für US-01 :)</p>
+    </div>
+  )
+}
+
+export default App
